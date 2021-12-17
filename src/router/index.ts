593b3d18afdf8312ffe6367c66bdaff8e7be25ee
饶长春 <raochangchun@mucang.cn>
2021-12-17 09:47:34 +0800

    更改为哈希路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import * as Router from "vue-router";
 import { constantRoutes, asyncRoutes } from "./routes";
 
 const router = Router.createRouter({
-    history: Router.createWebHistory(),
+    history: Router.createWebHashHistory(),
     routes: constantRoutes.concat(asyncRoutes)
 })
 
