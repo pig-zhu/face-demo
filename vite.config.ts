e1c6705bfa3a3c802cf373167ae989dc0dc009d3
饶长春 <raochangchun@mucang.cn>
2021-12-21 10:41:51 +0800

    配置路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 const {resolve} = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/face/',
   plugins: [vue()],
 	resolve: {
     alias: {
